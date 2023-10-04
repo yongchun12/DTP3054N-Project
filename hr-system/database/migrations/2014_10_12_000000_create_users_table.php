ef64fb54c997fc74ee72f9df3224a997f36f3105
yongchun <yongchun_sam@hotmail.com>
2023-10-04 16:52:16 +0800

    Create Laravel

--- hr-system/database/migrations/2014_10_12_000000_create_users_table.php
+++ hr-system/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+
+            #unique() method is used to avoid same data in the list of row or column
             $table->string('email')->unique();
+
+            #nullable() method is used to allow null values for the column
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ hr-system/resources/views/login.blade.php
@@ -0,0 +1,89 @@
+<!--
+    Template: https://mdbootstrap.com/docs/standard/extended/login/
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!--Header-->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Boostrap 5 CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/app.css">
+    <title>HR System | Login</title>
+</head>
+
+<!--Body-->
+<body>
+
+<section class="vh-100" style="background-color: #e9ecef;">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+
+            <!--Size of Center-->
+            <div class="col col-xl-10">
+                <div class="card" style="border-radius: 1rem;">
+                    <div class="row g-0">
+
+                        <!--Left Site-->
+                        <div class="col-md-6 col-lg-5 d-md-block">
+                            <img src="img/pexels-mikhail-nilov-8297030.jpg"
+                                 alt="Wallpaper" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                        </div>
+                        <div class="col-md-6 col-lg-7 d-flex">
+                            <div class="card-body p-4 p-lg-5 text-black">
+
+                                <!--Action in this form section-->
+                                <form action="login.php" method="post">
+
+                                    <?php if(isset($_GET['error'])) { ?>
+                                        <div class="alert alert-danger" role="alert">
+                                            <?php echo $_GET['error']; ?>
+                                        </div>
+                                    <?php } ?>
+
+                                    <!--Logo-->
+                                    <div class="mb-4">
+                                        <img src="img/Project%20Logo.png" alt="Logo" width="100" height="100">
+                                    </div>
+
+                                    <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
+
+                                    <div class="form-outline mb-4">
+                                        <label class="form-label">Email address</label>
+                                        <input type="text" name="username" id="form2Example17" class="form-control form-control-lg" />
+                                    </div>
+
+                                    <div class="form-outline mb-4">
+                                        <label class="form-label">Password</label>
+                                        <input type="password" name="password" id="form2Example27" class="form-control form-control-lg" />
+                                    </div>
+
+                                    <div class="pt-1 mb-3 d-grid">
+                                        <button class="btn btn-dark btn-lg btn-block" type="submit">Login</button>
+                                    </div>
+
+                                </form>
+
+                                <a class="small text-muted" href="forget_password.php">Forgot password?</a>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--Bootstrap-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
