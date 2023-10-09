5e8765c8ee9fae4cc1e0ca8d720afb54fd5eed17
yongchun <yongchun_sam@hotmail.com>
2023-10-09 17:02:17 +0800

    CDN Link Setting Updated

--- hr-system/resources/views/layouts/plugins.blade.php
+++ hr-system/resources/views/layouts/plugins.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>HR System | Dashboard</title>
 
+    <!--External CSS File-->
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
@@ -14,32 +15,36 @@
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 
-    <!-- Tempusdominus Bootstrap 4 -->
-{{--    <link rel="stylesheet" href="{{ asset('backend/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">--}}
-
-    <!--Bootstrap CDN Link-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
     <!-- iCheck -->
     <link rel="stylesheet" href="{{ asset('backend/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
 
     <!-- JQVMap -->
     <link rel="stylesheet" href="{{ asset('backend/plugins/jqvmap/jqvmap.min.css') }}">
 
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('backend/dist/css/adminlte.min.css') }}">
+    <!-- AdminLTE Theme style -->
+{{--    <link rel="stylesheet" href="{{ asset('backend/dist/css/adminlte.min.css') }}">--}}
 
     <!-- overlayScrollbars -->
     <link rel="stylesheet" href="{{ asset('backend/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
 
     <!-- Daterange picker -->
-{{--    <link rel="stylesheet" href="{{ asset('backend/plugins/daterangepicker/daterangepicker.css') }}">--}}
+    <link rel="stylesheet" href="{{ asset('backend/plugins/daterangepicker/daterangepicker.css') }}">
 
-    <!-- Daterange picker CDN Link -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <!-- Tempusdominus Bootstrap 4-->
+    <link rel="stylesheet" href="{{ asset('backend/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
 
     <!-- summernote -->
     <link rel="stylesheet" href="{{ asset('backend/plugins/summernote/summernote-bs4.min.css') }}">
+
+    <!--CSS CDN Link-->
+    <!--Bootstrap CDN Link-->
+{{--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">--}}
+
+{{--    <!-- Daterange picker CDN Link -->--}}
+{{--    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />--}}
+
+    <!--AdminLTE CSS CDN Link-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
 
 </head>
 
@@ -48,6 +53,7 @@
 
     @include('layouts.sidebar')
 
+    <!--Content-->
         @yield('content')
 
     @include('layouts.footer')
@@ -56,18 +62,13 @@
     <aside class="control-sidebar control-sidebar-dark">
         <!-- Control sidebar content goes here -->
     </aside>
-    <!-- /.control-sidebar -->
+    <!--/control-sidebar -->
 </div>
-<!-- ./wrapper -->
+<!--/wrapper -->
 
+<!--External JavaScript File-->
 <!-- jQuery -->
-{{--<script src="{{ asset('backend/plugins/jquery/jquery.min.js') }}"></script>--}}
-
-<!--jQuery CDN Link-->
-<script type="text/javascript" src="{{ asset('https://cdn.jsdelivr.net/jquery/latest/jquery.min.js') }}"></script>
-
-<!--Moment.js CDN Link-->
-<script type="text/javascript" src="{{ asset('https://cdn.jsdelivr.net/momentjs/latest/moment.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/jquery/jquery.min.js') }}"></script>
 
 <!-- jQuery UI 1.11.4 -->
 <script src="{{ asset('backend/plugins/jquery-ui/jquery-ui.min.js') }}'"></script>
@@ -94,17 +95,11 @@
 <script src="{{ asset('backend/plugins/jquery-knob/jquery.knob.min.js') }}"></script>
 
 <!-- daterangepicker -->
-{{--<script src="{{ asset('backend/plugins/moment/moment.min.js') }}"></script>--}}
-{{--<script src="{{ asset('backend/plugins/daterangepicker/daterangepicker.js') }}"></script>--}}\
-
-<!--Daterangepicker CDN Link-->
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+<script src="{{ asset('backend/plugins/moment/moment.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/daterangepicker/daterangepicker.js') }}"></script>\
 
 <!-- Tempusdominus Bootstrap 4 -->
-{{--<script src="{{ asset('backend/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>--}}
-
-<!--BootStrap CDN Link Javascript-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="{{ asset('backend/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
 
 <!-- Summernote -->
 <script src="{{ asset('backend/plugins/summernote/summernote-bs4.min.js') }}"></script>
@@ -113,13 +108,29 @@
 <script src="{{ asset('backend/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 
 <!-- AdminLTE App -->
-<script src="{{ asset('backend/dist/js/adminlte.js') }}"></script>
+{{--<script src="{{ asset('backend/dist/js/adminlte.js') }}"></script>--}}
 
 {{--<!-- AdminLTE for demo purposes -->--}}
 {{--<script src="{{ asset('backend/dist/js/demo.js') }}"></script>--}}
 
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{ asset('backend/dist/js/pages/dashboard.js') }}"></script>
+{{--<!-- AdminLTE dashboard demo (This is only for demo purposes) -->--}}
+{{--<script src="{{ asset('backend/dist/js/pages/dashboard.js') }}"></script>--}}
+
+<!--CDN Link JavaScript-->
+<!--AdminLTE CDN Link JavaScript-->
+<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+
+{{--<!--BootStrap CDN Link Javascript-->--}}
+{{--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>--}}
+
+{{--<!--Daterangepicker CDN Link-->--}}
+{{--<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>--}}
+
+{{--<!--jQuery CDN Link-->--}}
+{{--<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>--}}
+
+{{--<!--Moment.js CDN Link-->--}}
+{{--<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>--}}
 
 @yield('script')
 
