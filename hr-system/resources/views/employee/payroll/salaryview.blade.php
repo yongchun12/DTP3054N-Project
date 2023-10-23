da36f4628502dbe3e9977aab4caa0b071bc36dfc
Yong Chun <yongchun_sam@hotmail.com>
2023-10-23 14:45:38 +0800

    Fix: - Payslip view (Employee Site)

--- hr-system/resources/views/employee/payroll/salaryview.blade.php
+++ hr-system/resources/views/employee/payroll/salaryview.blade.php
@@ -24,8 +24,33 @@
     ?>
 
     <div class="content-wrapper">
-        <div class="container mt-5 mb-5">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row">
+
+                    <!--Back Button-->
+                    <div class="col-sm-6">
+                        <a href="{{ url('admin/payroll') }}" class="btn btn-primary d-print-none">Back</a>
+                    </div>
+
+                    <!--Print Button-->
+                    <div class="col-sm-6" style="text-align: right">
+                        <div class="btn btn-secondary" onclick="window.print()">Print</div>
+                        <a class="btn btn-secondary" id="download">Download PDF</a>
+                    </div>
+
+                    <!--Line-->
+                    <div class="col-md-12">
+                        <hr style="border: 0.1px solid black">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="container" id="payslip">
             <div class="row">
+
                 <div class="col-md-12">
                     <div class="text-center lh-1 mb-2">
                         <h1 class="fw-bold">Payslip</h1> <span class="fw-normal">Payment slip for the month of {{ \Carbon\Carbon::parse($getRecord->created_at)->format('M') }} {{ \Carbon\Carbon::parse($getRecord->created_at)->format('Y') }}</span>
@@ -82,6 +107,8 @@
                                     </div>
                                 </div>
                             </div>
+
+                            <!--Position-->
                             <div class="row">
                                 <div class="col-md-6">
                                     <div>
@@ -93,7 +120,7 @@
                                             @elseif($value_employee->job_id == 2)
                                                 Accountant
                                             @endif
-                                        @endif($value_employee->id == $getRecord->employee_id)
+                                        @endif
                                     @endforeach
                                 </span>
                                     </div>
@@ -117,7 +144,96 @@
                                 </div>
 
                             </div>
+
+                            <!--Space-->
+                            <div class="row col-md-12"><br></div>
+
+                            <!--Bank Name-->
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div>
+                                <span class="fw-bolder">Bank Name :
+                                    @foreach($getEmployee as $value_employee)
+                                        {{ ($value_employee->id == $getRecord->employee_id) ? ($value_employee->bank_name) : '' }}
+                                    @endforeach
+                                </span>
+                                    </div>
+                                </div>
+
+                                <!--Bank Number-->
+                                <div class="col-md-6">
+                                    <div>
+                                <span class="fw-bolder">Bank Number :
+                                    @foreach($getEmployee as $value_employee)
+                                        {{ ($value_employee->id == $getRecord->employee_id) ? ($value_employee->bank_acc) : '' }}
+                                    @endforeach
+                                </span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!--EPF No-->
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div>
+                                <span class="fw-bolder">EPF No :
+                                    @foreach($getEmployee as $value_employee)
+                                        {{ ($value_employee->id == $getRecord->employee_id) ? ($value_employee->epf_no) : '' }}
+                                    @endforeach
+                                </span>
+                                    </div>
+                                </div>
+
+                                <!--Bank Number-->
+                                <div class="col-md-6">
+                                    <div>
+                                <span class="fw-bolder">PCB No :
+                                    @foreach($getEmployee as $value_employee)
+                                        {{ ($value_employee->id == $getRecord->employee_id) ? ($value_employee->pcb_no) : '' }}
+                                    @endforeach
+                                </span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!--IC No-->
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div>
+                                <span class="fw-bolder">IC No :
+                                    @foreach($getEmployee as $value_employee)
+                                        {{ ($value_employee->id == $getRecord->employee_id) ? ($value_employee->ic_no) : '' }}
+                                    @endforeach
+                                </span>
+                                    </div>
+                                </div>
+
+                                <!--Employee Category-->
+                                <div class="col-md-6">
+                                    <div>
+                                <span class="fw-bolder">Category :
+                                    @foreach($getEmployee as $value_employee)
+                                        @if($value_employee->id == $getRecord->employee_id)
+                                            @if($value_employee->category_employee == 0)
+                                                Full Time
+                                            @elseif($value_employee->category_employee == 1)
+                                                Part Time
+                                            @elseif($value_employee->category_employee == 2)
+                                                Contract
+                                            @elseif($value_employee->category_employee == 3)
+                                                Temporary
+                                            @endif
+                                        @endif
+                                    @endforeach
+                                </span>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
+
+                        <!--Table-->
+
                         <table class="mt-4 table table-bordered">
                             <thead class="bg-dark text-white">
                             <tr>
@@ -134,6 +250,8 @@
                                     RM
                                     {{ $getRecord->gross_salary }}
                                 </td>
+                                <td></td>
+                                <td></td>
                             </tr>
                             <tr>
                                 <th scope="row">Number of Working Days</th>
@@ -165,6 +283,16 @@
                                 <td></td>
                                 <td></td>
                             </tr>
+                            {{--                    <tr>--}}
+                            {{--                        <th scope="row">Commission</th>--}}
+                            {{--                        <td>RM--}}
+                            {{--                            @foreach($getEmployee as $value_employee)--}}
+                            {{--                                {{ ($value_employee->id == $getRecord->employee_id) ? ($value_employee->commission) : '' }}--}}
+                            {{--                            @endforeach--}}
+                            {{--                        </td>--}}
+                            {{--                        <td></td>--}}
+                            {{--                        <td></td>--}}
+                            {{--                    </tr>--}}
 
                             <tr class="border-top">
                                 <th scope="row">Total Earning</th>
@@ -183,8 +311,32 @@
                         <span class="fw-bold">For HR System Authorised Signature</span>
                     </div>
                 </div>
+
+                <!--Space-->
+                <div class="row col-md-12"><br></div>
+
             </div>
         </div>
     </div>
+
+    <!--PDF Button-->
+    <script>
+        window.onload = function () {
+            document.getElementById("download")
+                .addEventListener("click", () => {
+                    const payslip = this.document.getElementById("payslip");
+                    console.log(payslip);
+                    console.log(window);
+                    var opt = {
+                        margin:       1,
+                        filename:     'payslip.pdf',
+                        image:        { type: 'jpeg', quality: 0.98 },
+                        html2canvas:  { scale: 2 },
+                        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+                    };
+                    html2pdf().from(payslip).set(opt).save();
+                })
+        }
+    </script>
 
 @endsection
